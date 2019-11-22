3288e9a01867fba54f0f3fea702a485d913e4642
Conlan Rios <conlan@gmail.com>
2019-11-21 23:28:31 -0800

    Track number of documents per ERC721 token key

--- contracts/Scribe.sol
+++ contracts/Scribe.sol
@@ -1,19 +1,53 @@
 pragma solidity ^0.5.12;
 
+library ConcatHelper {
+    function concat(bytes memory a, bytes memory b)
+            internal pure returns (bytes memory) {
+        return abi.encodePacked(a, b);
+    }
+}
+
 contract Scribe {
+	event Record (
+		address dictator,
+        address tokenAddress,
+        uint tokenId,
+        string text
+    );
+
 	struct Document {
 		address dictator;
 		string text;
 		uint time;
 	}
 
-	mapping (address => Document[]) public documents;
+	mapping (bytes => Document[]) public documents;
+	mapping (bytes => uint) public documentsCount;
 
-	constructor() public {
+	function dictate(address _tokenAddress, uint256 _tokenId, string memory _text) public {
+		// TODO check that msg.sender is owner of ERC721 at address + token Id
+		bytes memory documentKey = getDocumentKey(_tokenAddress, _tokenId);
+
+		documents[documentKey].push(Document(msg.sender, _text, block.timestamp));
+
+		documentsCount[documentKey]++;
+
+		emit Record(msg.sender, _tokenAddress, _tokenId, _text);
 	}
 
-	function dictate(string memory _text) public {
-		// TODO take an ERC721 address and tokenID
-		documents[msg.sender].push(Document(msg.sender, _text, block.timestamp));
+	function getDocumentKey(address _tokenAddress, uint256 _tokenId) public pure returns (bytes memory) {
+		return ConcatHelper.concat(toBytes(_tokenAddress), toBytes(_tokenId));
+	}
+
+	function toBytes(address x) internal pure returns (bytes memory b) { 
+		b = new bytes(20); 
+	
+		for (uint i = 0; i < 20; i++) 
+			b[i] = byte(uint8(uint(x) / (2**(8*(19 - i))))); 
+	}
+
+	function toBytes(uint256 x) internal pure returns (bytes memory b) {
+    	b = new bytes(32);
+    	assembly { mstore(add(b, 32), x) }
 	}
 }
