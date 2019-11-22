bfcc6b7d93aa4797052893ecd366ba0457c6e7a6
Conlan Rios <conlan@gmail.com>
2019-11-21 21:45:40 -0800

    Take a text parameter for dictation

--- contracts/Scribe.sol
+++ contracts/Scribe.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.5.12;
 contract Scribe {
 	struct Document {
 		address dictator;
-		bytes32 text;
+		string text;
 		uint time;
 	}
 
 	mapping (address => Document[]) public documents;
 
 	constructor() public {
-
 	}
 
-	function dictate() public {
-		documents[msg.sender].push(Document(msg.sender, "hello", block.timestamp));
+	function dictate(string memory _text) public {
+		// TODO take an ERC721 address and tokenID
+		documents[msg.sender].push(Document(msg.sender, _text, block.timestamp));
 	}
 }
