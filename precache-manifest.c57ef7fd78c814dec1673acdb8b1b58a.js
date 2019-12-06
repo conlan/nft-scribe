1059e3875da7d809ff44ec254a0cb6e112036a02
Conlan Rios <conlan@gmail.com>
2019-12-06 11:16:25 -0800

    Updates

---
+++ precache-manifest.c57ef7fd78c814dec1673acdb8b1b58a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6414d4ad720a76729318fef09cf2eef",
+    "url": "/nft-scribe/index.html"
+  },
+  {
+    "revision": "b8e3db8283e4399271ac",
+    "url": "/nft-scribe/static/css/main.5ebd4c27.chunk.css"
+  },
+  {
+    "revision": "31e5343353b10271938a",
+    "url": "/nft-scribe/static/js/2.3520d70c.chunk.js"
+  },
+  {
+    "revision": "b8e3db8283e4399271ac",
+    "url": "/nft-scribe/static/js/main.0983418c.chunk.js"
+  },
+  {
+    "revision": "ec44bf5ad2592ca22443",
+    "url": "/nft-scribe/static/js/runtime-main.9db26acb.js"
+  }
+]);
