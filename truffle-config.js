6bb27c81874ce3c67fb945b80f236055ab8156b0
Conlan Rios <conlan@gmail.com>
2019-12-06 11:31:42 -0800

    update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,38 +1,41 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const mnemonic = "";
 
-// module.exports = {
-//   // See <http://truffleframework.com/docs/advanced/configuration>
-//   // for more about customizing your Truffle configuration!
-//   networks: {
-//     development: {
-//       host: "127.0.0.1",
-//       port: 7545,
-//       network_id: "*" // Match any network id
-//     },
-//     develop: {
-//       port: 8545
-//     },
-//     compilers: {
-//       solc: {
-//         version: "^0.5.12" // replace w/ the solc version you need
-//       }
-//     }
-//   }
-// };
+const mnemonic = "";
 
 module.exports = {
   networks: {
+    "live": {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/")
+      },
+      network_id: 1
+    },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "")
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/")
       },
       network_id: 3
+    },
+    goerli: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/")
+      },
+      network_id: '5', // eslint-disable-line camelcase
+      gas: 4465030,
+      gasPrice: 10000000000,
+    },
+    compilers: {
+      solc: {
+        version: "^0.5.12+commit.7709ece9.Emscripten.clang" // replace w/ the solc version you need
+      }
     }
   },
   plugins: [
     'truffle-plugin-verify'
   ],
+  verify: {
+    preamble: "Author: Conlan Rios.\nVersion: 1.0\nSource: https://github.com/conlan/nft-scribe"
+  },
   api_keys: {
     etherscan: ''
   }
